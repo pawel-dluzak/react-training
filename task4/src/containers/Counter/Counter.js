7d32aa50ff46ffefc72a7b13a9e93bf9ef5c758d
Pawel Dluzak <pawel.dluzak@yahoo.com>
2020-12-05 11:43:57 +0100

    task4

---
+++ task4/src/containers/Counter/Counter.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import CounterControl from '../../components/CounterControl/CounterControl';
+import CounterOutput from '../../components/CounterOutput/CounterOutput';
+
+class Counter extends Component {
+    state = {
+        counter: 0
+    }
+    
+    render () {
+        return (
+            <div>
+                <CounterOutput value={this.props.ctr} />
+                <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
+                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
+                <CounterControl label="Add 5" clicked={this.props.onAddCounter}  />
+                <CounterControl label="Subtract 5" clicked={this.props.onSubCounter}  />
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        ctr: state.counter
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onIncrementCounter: () => dispatch({type: 'INCREMENT'}),
+        onDecrementCounter: () => dispatch({type: 'DECREMENT'}),
+        onAddCounter: () => dispatch({type:'ADD'}),
+        onSubCounter: () => dispatch({type:'SUB'})
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
