cf4201cac8d8a8b959b2b19b9556521708fef2e8
Pawel Dluzak <pawel.dluzak@yahoo.com>
2020-12-05 22:54:50 +0100

    task5

---
+++ task5/src/containers/Persons.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+import Person from '../components/Person/Person';
+import AddPerson from '../components/AddPerson/AddPerson';
+import { connect } from 'react-redux';
+
+class Persons extends Component {
+
+    render () {
+        return (
+            <div>
+                <AddPerson personAdded={this.props.onAddPerson} />
+                {this.props.person.map(person => (
+                    <Person 
+                        key={person.id}
+                        name={person.name} 
+                        age={person.age} 
+                        clicked={() => this.props.onDeletePerson(person.id)}/>
+                ))}
+            </div>
+        );
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+    onAddPerson: () => dispatch({type: 'ADD'}),
+    onDeletePerson: (id) => dispatch({type: 'DELETE', personId:id}),
+    };
+}
+
+const mapStateToProps = state => {
+    return {
+        person: state.person
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Persons);
